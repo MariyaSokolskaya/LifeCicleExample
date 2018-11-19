4307e9f21e0334b0682d22698556b3c09ee19f39
MariyaSokolskaya <faenna@inbox.ru>
2018-11-19 17:33:35 +0700

    Buttons

--- app/src/main/java/com/example/admin215/lifecicleexample/MainActivity.java
+++ app/src/main/java/com/example/admin215/lifecicleexample/MainActivity.java
@@ -2,17 +2,65 @@ package com.example.admin215.lifecicleexample;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button plusButton, minusButton;
+    EditText textA, textB;
+    TextView rezult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        plusButton = (Button) findViewById(R.id.plusbutton);
+        minusButton = (Button) findViewById(R.id.minesbutton);
+        textA = (EditText) findViewById(R.id.text1);
+        textB = (EditText) findViewById(R.id.text2);
+        rezult = (TextView) findViewById(R.id.rezult);
+
         Toast.makeText(this, "Я создана!", Toast.LENGTH_SHORT).show();
 
+/*        plusButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String aStr, bStr;
+                int a, b, c;
+                aStr = textA.getText().toString();
+                bStr = textB.getText().toString();
+                a = Integer.parseInt(aStr);
+                b = Integer.parseInt(bStr);
+                c = a + b;
+                rezult.setText(Integer.valueOf(c).toString());
+            }
+        });*/
 
+    }
+
+    void calculation(View v){
+        int id = v.getId();
+        String aStr, bStr;
+        int a, b, c = 0;
+        aStr = textA.getText().toString();
+        bStr = textB.getText().toString();
+        
+        a = Integer.parseInt(aStr);
+        b = Integer.parseInt(bStr);
+        switch (id){
+            case R.id.plusbutton:
+                c = a + b; break;
+            case R.id.minesbutton:
+                c = a - b; break;
+            default:
+                c = 0; break;
+        }
+        rezult.setText(Integer.valueOf(c).toString());
     }
 
     @Override
